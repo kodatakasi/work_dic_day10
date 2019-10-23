926d27d9f37a19462075d2fbadc070b6afa79085
Yu Sakita <sakitakitakita@gmail.com>
2019-10-23 14:56:05 +0900

    ログイン機能を作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  include SessionsHelper
+  
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = 'ログアウトしました'
+    redirect_to new_session_path
+  end
 end
